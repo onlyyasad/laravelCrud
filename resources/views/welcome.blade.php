4e6a90109a955fa72272e49ffa636aef78af71e0
hafij <hafij.alasad23@gmail.com>
2024-11-30 15:49:19 +0600

    ssh connect from ubuntu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,6 +59,7 @@
                               </tr>
                             @endforeach;
                           
+                            
                         </tbody>
                       </table>
                     </div>
